d9ff3c96cb691b61f345681c33f8d65f3729f3b8
samuelvelez <svelez0@gmail.com>
2022-03-16 14:11:25 -0500

    Initial Commit

---
+++ GameStream/GameStreamApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameStreamApp.swift
+//  GameStream
+//
+//  Created by Samuel on 16/3/22.
+//
+
+import SwiftUI
+
+@main
+struct GameStreamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
